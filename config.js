24a4d3859e567f3be6c505f66f52ff9ccbb58fc1
sarwagya999 <sarwagyaitsc@gmail.com>
2026-02-09 11:02:40 +0530

    Update config.js

--- config.js
+++ config.js
@@ -79,7 +79,8 @@ const CONFIG = {
     music: {
         enabled: true,                     // Music feature is enabled
         autoplay: true,                    // Try to autoplay (note: some browsers may block this)
-        musicUrl: "https://assets.mixkit.co/music/preview/mixkit-romantic-emotional-piano-668.mp3", // Music streaming URL
+        musicUrl: "https://assets.mixkit.co/music/preview/mixkit-romantic-emotional-piano-668.mp3"
+// Music streaming URL
         startText: "🎵 Play Music",        // Button text to start music
         stopText: "🔇 Stop Music",         // Button text to stop music
         volume: 0.5                        // Volume level (0.0 to 1.0)
